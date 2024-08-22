24503fe6a4b64ddda3be7ba6c989e244ca71246c
jonanv <jonan-vargas23@hotmail.com>
2024-08-22 16:19:03 -0500

    :truck: (root): Move test to folder tests

--- tests/app/root/app.component.spec.ts
+++ tests/app/root/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from '../../../src/app/root/app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -10,26 +10,27 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent
-      ],
+      ]
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'test-jest-angularjs'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-jest-angularjs');
+    const component = fixture.componentInstance;
+    expect(component.title).toEqual('test-jest-angularjs');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, test-jest-angularjs');
+    expect(compiled.querySelector('h1')?.textContent)
+      .toContain('Hello, test-jest-angularjs');
   });
 });
