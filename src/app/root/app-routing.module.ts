0c84ee3ad30141130e60e702e9781e9aa49d8dc5
jonanv <jonan-vargas23@hotmail.com>
2024-08-22 18:13:09 -0500

    :sparkles: (routing): Add routing for app

--- src/app/root/app-routing.module.ts
+++ src/app/root/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { CounterComponent } from '../modules/basic/components/counter/counter.component';
+
+const routes: Routes = [
+  {
+    path: 'basic/counter',
+    component: CounterComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'basic/counter'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
