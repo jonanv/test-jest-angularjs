f95e1a9adaf81c75cabe918ae26f6ffd221d46ca
jonanv <jonan-vargas23@hotmail.com>
2024-08-23 17:08:32 -0500

    :white_check_mark: (pokemon.service): Add test for pokemon service

--- tests/modules/basic/services/pokemon.service.spec.ts
+++ tests/modules/basic/services/pokemon.service.spec.ts
@@ -1,16 +1,36 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
-import { PokemonService } from './pokemon.service';
+// Services
+import { PokemonService } from '../../../../src/app/modules/basic/services/pokemon.service';
+
+// Interfaces
+import { Pokemon } from '../../../../src/app/modules/basic/interfaces/pokemon.interface';
 
 describe('PokemonService', () => {
   let service: PokemonService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ]
+    });
     service = TestBed.inject(PokemonService);
   });
 
-  it('should be created', () => {
+  test('Debe ser creado el servicio pokemon', () => {
     expect(service).toBeTruthy();
   });
+
+  test('Debe de traer informacion de Bulbasaur', (done) => {
+    service.getPokemon(1)
+      .subscribe((pokemon: Pokemon) => {
+        // console.log(pokemon.name);
+        expect(pokemon.name)
+          .toBe('bulbasaur');
+        done();
+      });
+  });
+
 });
