4de36abdd9f0104d24001920f7a6be094c2c2db9
jonanv <jonan-vargas23@hotmail.com>
2024-09-03 11:50:11 -0500

    :art: (father.component): Create estructure for component

--- src/app/modules/basic/components/father/father.component.ts
+++ src/app/modules/basic/components/father/father.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+// Interfaces
+import { Client } from '../../interfaces/client.interface';
+
 @Component({
   selector: 'app-father',
   templateUrl: './father.component.html',
   styles: ``
 })
 export class FatherComponent {
+
+  public client!: Client | undefined;
+
+  public onSetClient(name: string): void {
+    this.client = {
+      id: 1,
+      name
+    };
+  }
+
+  public processOnDeleteClientEmitter(): void {
+    this.client = undefined;
+  }
+
+  public processOnUpdateClientEmitter(client: Client): void {
+    this.client = client;
+  }
 
 }
