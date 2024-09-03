8e07485c42a0c9b77d728c197d0b63d827f2bc15
jonanv <jonan-vargas23@hotmail.com>
2024-09-03 10:15:14 -0500

    :sparkles: (father.component): Create father component

--- src/app/modules/basic/basic.module.ts
+++ src/app/modules/basic/basic.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 // Components
 import { CounterComponent } from './components/counter/counter.component';
 import { CharizardComponent } from './components/charizard/charizard.component';
+import { FatherComponent } from './components/father/father.component';
 
 @NgModule({
   declarations: [
     CounterComponent,
-    CharizardComponent
+    CharizardComponent,
+    FatherComponent,
   ],
   imports: [
     CommonModule,
---
+++ src/app/modules/basic/components/father/father.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-father',
+  templateUrl: './father.component.html',
+  styles: ``
+})
+export class FatherComponent {
+
+}
