50ae637cec9f367d560a058fe8bbfa02e1525c86
jonanv <jonan-vargas23@hotmail.com>
2024-09-03 15:15:44 -0500

    :truck: (counter-route): Move counter-route file to tests folder

--- src/tests/modules/basic/components/counter-route/counter-route.component.spec.ts
+++ src/tests/modules/basic/components/counter-route/counter-route.component.spec.ts
@@ -1,23 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CounterRouteComponent } from './counter-route.component';
+// Components
+import { CounterRouteComponent } from '../../../../../app/modules/basic/components/counter-route/counter-route.component';
+
 
 describe('CounterRouteComponent', () => {
   let component: CounterRouteComponent;
   let fixture: ComponentFixture<CounterRouteComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CounterRouteComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(CounterRouteComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
-  it('should create', () => {
+  test('Debe de crear el componente', () => {
     expect(component).toBeTruthy();
   });
+
 });
