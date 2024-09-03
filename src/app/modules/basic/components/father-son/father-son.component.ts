834846a43ad22a3a7cb9726dda7a7f88a843566a
jonanv <jonan-vargas23@hotmail.com>
2024-09-03 11:50:39 -0500

    :art: (father-son.component): Create structure for component

--- src/app/modules/basic/components/father-son/father-son.component.ts
+++ src/app/modules/basic/components/father-son/father-son.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+// Interfaces
+import { Client } from '../../interfaces/client.interface';
 
 @Component({
   selector: 'app-father-son',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class FatherSonComponent {
+
+  @Input() public client!: Client | undefined;
+  @Output() public onDeleteClientEmitter: EventEmitter<undefined> = new EventEmitter<undefined>();
+  @Output() public onUpdateClientEmitter: EventEmitter<Client> = new EventEmitter<Client>();
+
+  public onUpdateClient(): void {
+    if (!this.client) return;
+    this.client = { ...this.client, id: Math.floor(Math.random() * 10) };
+    this.onUpdateClientEmitter.emit(this.client);
+  }
+
+  public onDeleteClient(): void {
+    this.client = undefined;
+    this.onDeleteClientEmitter.emit();
+  }
 
 }
