d05ff7c490e90d3805e64f6170850ab0480dabe0
jonanv <jonan-vargas23@hotmail.com>
2024-09-03 15:51:17 -0500

    :white_check_mark: (counter-route): Add test for counter-route implementing params

--- src/tests/modules/basic/components/counter-route/counter-route.component.spec.ts
+++ src/tests/modules/basic/components/counter-route/counter-route.component.spec.ts
@@ -1,7 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 // Components
 import { CounterRouteComponent } from '../../../../../app/modules/basic/components/counter-route/counter-route.component';
+import { ActivatedRoute } from '@angular/router';
 
 
 describe('CounterRouteComponent', () => {
@@ -9,9 +11,10 @@ describe('CounterRouteComponent', () => {
   let fixture: ComponentFixture<CounterRouteComponent>;
   let compiled: HTMLElement;
 
-  beforeEach(async () => {
+  test('Debe de tener el valor inicial en cero', async() => {
     await TestBed.configureTestingModule({
-      declarations: [CounterRouteComponent]
+      declarations: [CounterRouteComponent],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
 
@@ -19,10 +22,62 @@ describe('CounterRouteComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
     compiled = fixture.nativeElement as HTMLElement;
+
+    expect(component.counter).toBe(0);
   });
 
-  test('Debe de crear el componente', () => {
-    expect(component).toBeTruthy();
+  test('Debe de tener el valor 100 en la ruta /counter/100', async() => {
+    const mockActivatedRoute = {
+      snapshot: {
+        paramMap: {
+          get(param: string) {
+            return (param === 'initial') ? '100' : undefined;
+          }
+        }
+      }
+    }
+
+    await TestBed.configureTestingModule({
+      declarations: [CounterRouteComponent],
+      providers: [{
+        provide: ActivatedRoute, useValue: mockActivatedRoute
+      }],
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CounterRouteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled = fixture.nativeElement as HTMLElement;
+
+    expect(component.counter).toBe(100);
+  });
+
+  test('Debe de tener el valor 10 en la ruta /counter/20abc', async() => {
+    const mockActivatedRoute = {
+      snapshot: {
+        paramMap: {
+          get(param: string) {
+            return (param === 'initial') ? '20abc' : undefined;
+          }
+        }
+      }
+    }
+
+    await TestBed.configureTestingModule({
+      declarations: [CounterRouteComponent],
+      providers: [{
+        provide: ActivatedRoute, useValue: mockActivatedRoute
+      }],
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CounterRouteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled = fixture.nativeElement as HTMLElement;
+
+    expect(component.counter).toBe(10);
   });
 
 });
